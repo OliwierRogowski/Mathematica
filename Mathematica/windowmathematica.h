fa2c6d83860a5e91c01de7f61e553842556676f0
OliwierRogowski <78658999+OliwierRogowski@users.noreply.github.com>
2025-03-23 13:53:03 +0100

    version 1.0

---
+++ Mathematica/windowmathematica.h
@@ -0,0 +1,40 @@
+#ifndef WINDOWMATHEMATICA_H
+#define WINDOWMATHEMATICA_H
+
+#include <QMainWindow>
+#include <QPlainTextEdit>
+#include <QPushButton>
+#include <QList>
+#include "functionobjects.h"
+#include "functionschartspoints.h"
+#include <QChartView>
+#include <QOpenGLWidget>
+#include <QOpenGLFunctions>
+
+class WindowMathematica : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    WindowMathematica(QWidget *parent = nullptr);
+    ~WindowMathematica();
+
+private slots:
+    void CheckFunction();
+
+private:
+    void Function();
+    void PaintFunction(const FunctionObject& func);
+
+    QPlainTextEdit *MainInput;
+    QPushButton *Wykonaj;
+
+    QList<FunctionObject> functions;  // Lista funkcji
+    QList<functionsChartsPoints> chartPoints;  // Lista punktów funkcji
+    QList<QPointF> points;  // Punkty funkcji do wykresu
+    double a, b, c;  // Współczynniki funkcji kwadratowej, do przykładu
+    double step = 0.1;  // Krok obliczania punktów
+    QChartView *previousChartView;
+};
+
+#endif // WINDOWMATHEMATICA_H
